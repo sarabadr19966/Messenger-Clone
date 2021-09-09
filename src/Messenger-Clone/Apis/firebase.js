c1bd4d14b4906b718736666e4c3bfc035cc8a399
sarabadr19966 <sarabadr19966@gmail.com>
2021-09-09 19:16:27 +0200

    first commit

--- src/Messenger-Clone/Apis/firebase.js
+++ src/Messenger-Clone/Apis/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore} from 'firebase/firestore/lite';
-/*
+
 const firebaseConfig = {
   apiKey: "AIzaSyD5ZgJ84YcCuBttiz-U1sYist3cUDaZHPQ",
   authDomain: "messenger-clone-2cda1.firebaseapp.com",
@@ -10,7 +10,7 @@ const firebaseConfig = {
   appId: "1:990958815139:web:ea5e7031d5fd17cd65fc16",
   measurementId: "G-CR2JZSLF0P"
 };
-*/
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyAeN0BC8UqxYPP37HAwVD1v7bqBfKzuddc",
   authDomain: "messenger-clone-a8cfc.firebaseapp.com",
@@ -19,7 +19,7 @@ const firebaseConfig = {
   messagingSenderId: "134384683923",
   appId: "1:134384683923:web:7f4b82e2dbbcd213b3f666"
 };
-
+*/
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
 
